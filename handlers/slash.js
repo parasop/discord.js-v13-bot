0871a8cdfd0111d82626945779e311392b452f6b
parasop <63485824+parasop@users.noreply.github.com>
2021-08-09 14:03:50 +0530

    Create slash.js

---
+++ handlers/slash.js
@@ -0,0 +1,33 @@
+module.exports = (client) => {
+    readdirSync("./SlashCommands/").forEach(dir => {
+        const commands = readdirSync(`./SlashCommands/${dir}/`).filter(file => file.endsWith(".js"));
+    
+        for (let file of commands) {
+            let pull = require(`../SlashCommands/${dir}/${file}`);
+    
+            if (pull.name) {
+                client.slash.set(pull.name, pull);
+                slash.push(pull);
+                table.addRow(file, '✅');
+            } else {
+                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+                continue;
+            }
+    
+            }
+    });
+    
+    console.log(table.toString());
+
+
+client.on("ready",async ()=> {
+
+
+
+//registering slash comand
+
+await client.application.commands.set(slash)
+
+})
+
+}
